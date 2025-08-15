954821f62d171b07d89303bbcca9b52afabad815
Dương Ngoc Y <duongocy@gmail.com>
2025-08-15 22:42:26 +0700

    lay them thong so 3

--- learning-english-api.js
+++ learning-english-api.js
@@ -6,6 +6,14 @@ const cron = require('node-cron');
 
 const app = express();
 app.use(cors()); // Sử dụng middleware CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Handle preflight request (OPTIONS)
+app.options('*', cors());
 app.use(express.json());
 
 // Cấu hình kết nối đến PostgreSQL
