a730fe49ede322d1df2370bbf8899e72bedc7507
Y Duong Ngoc <ydn@hoya.com>
2025-08-16 09:04:17 +0700

    api learning english 2

--- learning-english-api.js
+++ learning-english-api.js
@@ -51,7 +51,7 @@ app.get('/Invoice', async (req, res) => {
 });
 
 // Khởi động server
-const PORT = 3002;
+const PORT = 3003;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
